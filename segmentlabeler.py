77361d801e6a95d00eb643756c1bc60a9c87188a
Navid Nourani Vatani <n.nourani-vatani@acfr.usyd.edu.au>
2013-04-05 13:09:56 +1100

    hand coded python version

---
+++ segmentlabeler.py
@@ -0,0 +1,35 @@
+import cv
+
+def labelSegments( segImg ):
+	#cv.NamedWindow("seg")
+	
+	lblImg = cv.CreateMat( segImg.rows, segImg.cols, cv.CV_32F )
+	cv.Convert( segImg, lblImg )
+	
+	
+	mml = cv.MinMaxLoc( lblImg )
+	# No segments available!
+	if mml[1] == 0:
+		return lblImg
+	
+	# Scale to 0-MAX
+	cv.Scale( lblImg, lblImg, 2.**32/mml[1] )
+	
+	# Label each segment
+	mml = cv.MinMaxLoc( lblImg )
+	nextSegLbl = 0.
+	while( mml[0] == 0 ):
+		nextSegLbl += 1
+		cv.FloodFill( lblImg, mml[2], nextSegLbl )
+		
+		mml = cv.MinMaxLoc( lblImg )
+		
+		#showImg = lblImg
+		#cv.Scale( showImg, showImg, 1./nextSegLbl )
+		#print cv.MinMaxLoc( showImg ), '\n'
+		#cv.ShowImage("seg", showImg)
+		#cv.WaitKey(100)
+
+	cv.Set( lblImg, 0, segImg )
+	#cv.DestroyWindow('seg')
+	return lblImg
